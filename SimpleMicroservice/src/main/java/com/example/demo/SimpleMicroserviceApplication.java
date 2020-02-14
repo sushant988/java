7e82833d7859edce2530e67ae6b705cc8791179a
Vaddaram <svaddaram@deloitte.com>
2020-02-14 22:26:44 +0530

    sdd

--- SimpleMicroservice/src/main/java/com/example/demo/SimpleMicroserviceApplication.java
+++ SimpleMicroservice/src/main/java/com/example/demo/SimpleMicroserviceApplication.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@EnableConfigServer()
 public class SimpleMicroserviceApplication {
 	
 	@Autowired
