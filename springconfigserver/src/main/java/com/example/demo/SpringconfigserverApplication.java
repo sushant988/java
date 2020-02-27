da7027f9e86ea3ebe34feffbd9fa8935ed624c54
Vaddaram <svaddaram@deloitte.com>
2020-02-27 15:40:34 +0530

    commited

---
+++ springconfigserver/src/main/java/com/example/demo/SpringconfigserverApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringconfigserverApplication.class, args);
+	}
+
+}
